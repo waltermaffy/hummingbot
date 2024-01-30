4eab61222a6b148c54bfa2b93e6f2b2cf263c587
waltermaffy <walterm21@proton.me>
2024-01-30 22:48:04 +0100

    fix rest auth

--- hummingbot/connector/exchange/changelly/changelly_auth.py
+++ hummingbot/connector/exchange/changelly/changelly_auth.py
@@ -4,11 +4,11 @@ import json
 import time
 from base64 import b64encode
 from typing import Any, Dict, List, Optional
-from urllib.parse import urlsplit
-
+from urllib.parse import urlsplit, urlencode
+from collections import OrderedDict
 from hummingbot.connector.time_synchronizer import TimeSynchronizer
 from hummingbot.core.web_assistant.auth import AuthBase
-from hummingbot.core.web_assistant.connections.data_types import RESTRequest
+from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest, WSRequest
 
 
 class ChangellyAuth(AuthBase):
@@ -16,6 +16,7 @@ class ChangellyAuth(AuthBase):
         self.api_key = api_key
         self.secret_key = secret_key
         self.time_provider = time_provider
+        self.window = 50000
 
     async def rest_authenticate(self, request: RESTRequest) -> RESTRequest:
         """
@@ -51,26 +52,25 @@ class ChangellyAuth(AuthBase):
                 "signature": sign,
             },
         }
-
+    
     def header_for_authentication(self, request: RESTRequest) -> Dict[str, str]:
         # HS256 authentication as here https://api.pro.changelly.com/#hs256
         url = urlsplit(request.url)
-        message = [request.method, url.path]
+        message = [str(request.method), url.path]
         if url.query:
             message.append("?")
             message.append(url.query)
         if request.data:
             message.append(request.data)
 
-        print("message: ", message)
-        timestamp = str(int(self.time_provider.time() * 1e3))
+        timestamp = str(int(time.time() * 1000))
         message.append(timestamp)
-
+       
         signature = self._generate_signature(message)
         data = [self.api_key, signature, timestamp]
         base64_encoded = b64encode(":".join(data).encode("utf8")).decode("utf8")
         return {"Authorization": f"HS256 {base64_encoded}"}
 
     def _generate_signature(self, message: List[str]) -> str:
-        digest = hmac.new(self.secret_key.encode("utf8"), "".join(message).encode("utf8"), hashlib.sha256).hexdigest()
+        digest = hmac.new(self.secret_key.encode(), "".join(message).encode(), hashlib.sha256).hexdigest()
         return digest
